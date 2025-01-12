b8733731b22efb34b4934434236f8528d710c958
Kevin Campos Buendia <91388417+AlphaSKC@users.noreply.github.com>
2025-01-12 18:14:22 -0500

    FT: FLIGHT RESERVATIONS

--- src/components/home/ReservationsForm.tsx
+++ src/components/home/ReservationsForm.tsx
@@ -1,19 +1,20 @@
 import { Box, Button, Grid2 } from "@mui/material";
-import { useState } from "react";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Autocomplete, AutocompleteItem, Input } from "@nextui-org/react";
 
 //ICONOS
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 import FlightLandIcon from '@mui/icons-material/FlightLand';
-import ScheduleIcon from '@mui/icons-material/Schedule';
+import FlightIcon from '@mui/icons-material/Flight';
+// import ScheduleIcon from '@mui/icons-material/Schedule';
 
 //DATE PICKER
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+import { DateTimePicker, DateTimePickerProps } from '@mui/x-date-pickers/DateTimePicker';
+import { AccountCircle } from "@mui/icons-material";
 
 const places = [
     {
@@ -43,6 +44,37 @@ const places = [
     }
 ]
 
+const CustomDateTimePicker: React.FC<DateTimePickerProps<any>> = (props) => {
+    return (
+        <DateTimePicker
+            {...props}
+            sx={{
+                "& .MuiOutlinedInput-root": {
+                    borderRadius: "12px",
+                    backgroundColor: "white",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#ccc",
+                    transition: "border-color 0.3s ease",
+                },
+                "& .MuiOutlinedInput-root:not(.Mui-disabled):hover .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#e68a00",
+                },
+                "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#e68a00",
+                },
+
+                "& .MuiInputLabel-root": {
+                    color: "#52525b",
+                    "&.Mui-focused": {
+                        color: "#e68a00"
+                    },
+                },
+            }}
+        />
+    );
+}
+
 export default function ReservationsForm() {
     const [isSencillo, setIsSencillo] = useState(false);
 
@@ -52,12 +84,16 @@ export default function ReservationsForm() {
             flexDirection: "column",
             backgroundColor: "#f5f5f5",
             alignItems: "center",
+            minHeight: "60vh",
             height: "60vh",
             width: "100%",
             paddingY: "20px",
         }}>
             <Box sx={{
-                backgroundColor: 'blue',
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "blue",
+                justifyContent: "center",
                 width: "80%",
                 height: "100%",
                 paddingX: "20px",
@@ -83,7 +119,7 @@ export default function ReservationsForm() {
                     }}
                         onClick={() => setIsSencillo(false)}
                     >
-                        Viaje redondo
+                        Round Trip
                     </Button>
                     <Button sx={{
                         padding: "5px 10px",
@@ -96,17 +132,18 @@ export default function ReservationsForm() {
                     }}
                         onClick={() => setIsSencillo(true)}
                     >
-                        Viaje sencillo
+                        One Way
                     </Button>
                 </Box>
                 <Grid2 container spacing={0} width={"100%"} sx={{ marginTop: "20px" }}>
                     <Grid2 size={{ xs: 12, md: 5 }} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
                         <Autocomplete
-                            className="max-w-lg"
+                            className="max-w-lg "
                             defaultItems={places}
                             defaultSelectedKey={"CUN"}
-                            label="Origen"
-                            placeholder="¿De dónde sales?"
+                            label="Origin"
+                            placeholder="Where are you traveling from?"
+                            size="lg"
                             startContent={<FlightTakeoffIcon />}
                         >
                             {(item) => <AutocompleteItem key={item.key}>{item.name}</AutocompleteItem>}
@@ -120,15 +157,16 @@ export default function ReservationsForm() {
                             border: "1px solid #ccc",
                             color: "#595959",
                         }}>
-                            <SwapHorizIcon />
+                            <SwapHorizIcon sx={{ height: '5vh' }} />
                         </Button>
                     </Grid2>
                     <Grid2 size={{ xs: 12, md: 5 }} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Autocomplete
                             className="max-w-lg"
                             defaultItems={places}
-                            label="Destino"
-                            placeholder="Selecciona un destino"
+                            label="Destination"
+                            size="lg"
+                            placeholder="Where are you traveling to?"
                             startContent={<FlightLandIcon />}
                         >
                             {(item) => <AutocompleteItem key={item.id}>{item.name}</AutocompleteItem>}
@@ -136,45 +174,75 @@ export default function ReservationsForm() {
                     </Grid2>
                 </Grid2>
                 <Grid2 container spacing={2} width={"100%"} sx={{ marginTop: "20px" }}>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
+                    <Grid2 size={{ xs: 6 }}>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DemoContainer components={['DateTimePicker', 'DateTimePicker']}>
-                                <DateTimePicker
-                                    sx={{
-                                        backgroundColor: "white",
-                                        borderRadius: "5px",
-                                    }}
-                                    
-                                    slots={{openPickerIcon: ScheduleIcon}}
-                                    label="Salida"
-                                    viewRenderers={{
-                                        hours: renderTimeViewClock,
-                                        minutes: renderTimeViewClock,
-                                        seconds: renderTimeViewClock,
-                                    }}
-                                />
+                            <DemoContainer components={['DateTimePicker']}>
+                                <CustomDateTimePicker label="Departure" />
                             </DemoContainer>
                         </LocalizationProvider>
                     </Grid2>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
+                    <Grid2 size={{ xs: 6 }}>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DemoContainer components={['DateTimePicker', 'DateTimePicker']}>
-                                <DateTimePicker
-                                    sx={{
-                                        backgroundColor: "white",
-                                        borderRadius: "5px",
-                                    }}
-                                    label="Regreso"
-                                    viewRenderers={{
-                                        hours: renderTimeViewClock,
-                                        minutes: renderTimeViewClock,
-                                        seconds: renderTimeViewClock,
-                                    }}
-                                />
+                            <DemoContainer components={['DateTimePicker']}>
+                                <CustomDateTimePicker label="Return" disabled={isSencillo} />
                             </DemoContainer>
                         </LocalizationProvider>
                     </Grid2>
                 </Grid2>
+                <Grid2 container spacing={2} width={"100%"} sx={{ marginTop: "20px" }}>
+                    <Grid2 size={{ xs: 12, md: 6 }}>
+                        <Input
+                            classNames={{
+                                label: "text-black/50 dark:text-white/90",
+                                input: [
+                                    "bg-transparent",
+                                    "text-black/90 dark:text-white/90",
+                                    "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                                ],
+                                innerWrapper: "bg-transparent",
+                                inputWrapper: [
+                                    "shadow-xl",
+                                    "bg-default-200/50",
+                                    "dark:bg-default/60",
+                                    "backdrop-blur-xl",
+                                    "backdrop-saturate-200",
+                                    "hover:bg-default-200/70",
+                                    "dark:hover:bg-default/70",
+                                    "group-data-[focus=true]:bg-default-200/50",
+                                    "dark:group-data-[focus=true]:bg-default/60",
+                                    "!cursor-text",
+                                ],
+                            }}
+                            label="Passengers"
+                            type="number"
+                            placeholder="0"
+                            radius="lg"
+                            startContent={
+                                <AccountCircle />
+                            }
+                        />
+                    </Grid2>
+                </Grid2>
+                <Button
+                    sx={{
+                        width: "100%",
+                        backgroundColor: "#e68a00",
+                        borderRadius: "50px",
+                        padding: "15px",
+                        fontSize: "1.1rem",
+                        color: "white",
+                        fontWeight: "bold",
+                        textTransform: "none",
+                        marginTop: "20px",
+                        ":hover":{
+                            backgroundColor: "white",
+                            color: "#e68a00",
+                        }
+                    }}
+                >
+                    Quote Flight
+                    <FlightIcon sx={{ marginLeft: "10px" }} />
+                </Button>
             </Box>
         </Box>
     );
